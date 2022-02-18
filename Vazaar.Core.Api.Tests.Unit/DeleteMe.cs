1cd9c9fab2ff00a3a8dda4479d4cb4f576eca09a
yasirthite <yasir.thite@gmail.com>
2022-02-18 10:19:06 +0530

    INFRA: Initialize Unit Test Project
    Closes #2

---
+++ Vazaar.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace Vazaar.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
