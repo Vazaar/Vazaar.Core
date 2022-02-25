a8324ed9a7ead1a320727f635002b13fcbbc48a6
Hassan Habib <hassanhabib@live.com>
2022-02-24 20:56:02 -0800

    INFRA: Initialize Acceptance Tests

---
+++ Vazaar.Core.Api.Tests.Acceptance/APIs/Homes/HomeApiTests.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Vazaar.Core.Api.Tests.Acceptance.Brokers;
+using Xunit;
+
+namespace Vazaar.Core.Api.Tests.Acceptance.APIs.Homes
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class HomeApiTests
+    {
+        private readonly VazaarApiBroker vazaarApiBroker;
+
+        public HomeApiTests(VazaarApiBroker vazaarApiBroker) =>
+            this.vazaarApiBroker = vazaarApiBroker;
+
+        [Fact]
+        public async Task ShouldReturnHomeMessageAsync()
+        {
+            // given
+            string expectedMessage =
+                "Hello, Mario. The princesse is in another castle.";
+
+            // when
+            string actualMessage =
+                await this.vazaarApiBroker.GetHomeMessageAsync();
+
+            // then
+            actualMessage.Should().BeEquivalentTo(expectedMessage);
+        }
+    }
+}
---
+++ Vazaar.Core.Api.Tests.Acceptance/Brokers/VazaarApiBroker.Home.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace Vazaar.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class VazaarApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+
+        public async ValueTask<string> GetHomeMessageAsync() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
