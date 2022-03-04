14a727e1d6ddcca32f1fe1cac13d4b2568c53765
Hassan Habib <hassanhabib@live.com>
2022-03-03 20:42:16 -0800

    BROKERS: Create Resource Group

---
+++ Vazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.ResourceGroups.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IResourceGroup> CreateResourceGroupAsync(string resourceGroupName);
+    }
+}
