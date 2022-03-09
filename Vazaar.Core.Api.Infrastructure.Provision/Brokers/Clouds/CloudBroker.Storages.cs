fcfc33767c3cbc817387ca1109970573f64d83e8
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2022-03-09 10:11:27 +0500

    BROKERS: Implement SQL Server

---
+++ Vazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Storages.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Sql.Fluent;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<ISqlServer> CreateSqlServerAsync(
+            string sqlServerName,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.SqlServers
+                .Define(sqlServerName)
+                .WithRegion(Region.IndiaCentral)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithAdministratorLogin(this.adminName)
+                .WithAdministratorPassword(this.adminAccess)
+                .CreateAsync();
+        }
+    }
+}
--- Vazaar.Core.Api.Infrastructure.Provision/Program.cs
+++ Vazaar.Core.Api.Infrastructure.Provision/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Hello, World!");
