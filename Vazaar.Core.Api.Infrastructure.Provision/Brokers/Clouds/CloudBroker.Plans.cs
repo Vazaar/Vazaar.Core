924d5112f13c0aff1b9a917c6b875e85aa935c9d
shawn301287 <shawn301287@gmail.com>
2022-03-09 09:16:46 +0400

    BROKERS: Implement Service Plan

---
+++ Vazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Plans.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<IAppServicePlan> CreatePlanAsync(
+            string planName,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.AppServices.AppServicePlans
+                .Define(planName)
+                .WithRegion(Region.IndiaCentral)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithPricingTier(PricingTier.StandardS1)
+                .WithOperatingSystem(Microsoft.Azure.Management.AppService.Fluent.OperatingSystem.Windows)
+                .CreateAsync();
+        }
+    }
+}
---
+++ Vazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Plans.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IAppServicePlan> CreatePlanAsync(
+            string planName,
+            IResourceGroup resourceGroup);
+    }
+}
