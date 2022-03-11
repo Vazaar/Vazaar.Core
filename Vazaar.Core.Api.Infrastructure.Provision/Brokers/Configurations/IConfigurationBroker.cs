801b046d56ab276e45c21396e4c18b487907ca95
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2022-03-11 09:56:39 +0500

    BROKER: Implemented Configuration broker.

---
+++ Vazaar.Core.Api.Infrastructure.Provision/Brokers/Configurations/IConfigurationBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using Vazaar.Core.Api.Infrastructure.Provision.Models.Configurations;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Brokers.Configurations
+{
+    public interface IConfigurationBroker
+    {
+        CloudManagementConfiguration GetConfiguration();
+    }
+}
---
+++ Vazaar.Core.Api.Infrastructure.Provision/Models/Configurations/CloudAction.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudAction
+    {
+        public List<string> Environments { get; set; }
+    }
+}
