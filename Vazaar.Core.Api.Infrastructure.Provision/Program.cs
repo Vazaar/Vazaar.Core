b62ca57286c0cb2efb9c7844c5cbd2b7e4fe7443
shawn301287 <shawn301287@gmail.com>
2022-03-25 07:52:15 +0400

    EXPOSURE: Implement Processing Service

--- Vazaar.Core.Api.Infrastructure.Provision/Program.cs
+++ Vazaar.Core.Api.Infrastructure.Provision/Program.cs
@@ -1,3 +1,22 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Yasir Thite All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
 
-Console.WriteLine("Hello, World!");
+using System.Threading.Tasks;
+using Vazaar.Core.Api.Infrastructure.Provision.Services.Processings.CloudManagements;
+
+namespace Vazaar.Core.Api.Infrastructure.Provision
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ICloudManagementProcessingService cloudManagementProcessingService =
+               new CloudManagementProcessingService();
+
+            await cloudManagementProcessingService.ProcessAsync();
+        }
+    }
+}
